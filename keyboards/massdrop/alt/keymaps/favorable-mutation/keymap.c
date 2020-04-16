1739f94b73548b91df72ed1ef1849b20cfab64a8
Griffin J Rademacher <contact@griffin-rademacher.info>
2020-04-16 17:37:49 -0400

    rollback wm key

--- keyboards/massdrop/alt/keymaps/favorable-mutation/keymap.c
+++ keyboards/massdrop/alt/keymaps/favorable-mutation/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
         CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_MUTE,
         TP_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_VOLU, KC_MPLY,
-        TP_LALT, TG(2),   KC_YBAI,                            GUI_SPC,                            TP_RALT, TG(1),   KC_MRWD, KC_VOLD, KC_MFFD
+        TP_LALT, CTL_ESC, KC_LGUI,                            KC_SPC,                            TP_RALT, MO(1),   KC_MRWD, KC_VOLD, KC_MFFD
     ),
     [1] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL,
