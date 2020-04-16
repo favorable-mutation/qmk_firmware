aeeb53d231046ba57f2870514ae9f24c5d23aa5e
Griffin J Rademacher <contact@griffin-rademacher.info>
2020-04-16 17:46:09 -0400

    swaps ctl and alt

--- keyboards/massdrop/alt/keymaps/favorable-mutation/keymap.c
+++ keyboards/massdrop/alt/keymaps/favorable-mutation/keymap.c
@@ -12,7 +12,7 @@ enum alt_keycodes {
 };
 
 #define TG_NKRO  MAGIC_TOGGLE_NKRO //Toggle 6KRO / NKRO mode
-#define CTL_ESC  LCTL_T(KC_ESC)
+#define CTL_ESC  LALT_T(KC_ESC)
 #define TP_LSFT  OSM(MOD_LSFT)
 #define TP_RSFT  OSM(MOD_RSFT)
 #define TP_LALT  OSM(MOD_LALT)
@@ -34,9 +34,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_SPOT,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
-        CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_MUTE,
+        ALT_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_MUTE,
         TP_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_VOLU, KC_MPLY,
-        TP_LALT, CTL_ESC, KC_LGUI,                            KC_SPC,                            TP_RALT, MO(1),   KC_MRWD, KC_VOLD, KC_MFFD
+        KC_LCTL, ALT_ESC, KC_LGUI,                            KC_SPC,                            TP_RALT, MO(1),   KC_MRWD, KC_VOLD, KC_MFFD
     ),
     [1] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL,
