58bca8f9a2fde7d32ebdba9f93c9862739a08c5d
Griffin J Rademacher <griffin@radix.bio>
2020-05-04 14:22:14 -0400

    reverts some deviations from default board

--- keyboards/massdrop/alt/keymaps/favorable-mutation/keymap.c
+++ keyboards/massdrop/alt/keymaps/favorable-mutation/keymap.c
@@ -12,7 +12,7 @@ enum alt_keycodes {
 };
 
 #define TG_NKRO  MAGIC_TOGGLE_NKRO //Toggle 6KRO / NKRO mode
-#define CTL_ESC  LALT_T(KC_ESC)
+#define ALT_ESC  LALT_T(KC_ESC)
 #define TP_LSFT  OSM(MOD_LSFT)
 #define TP_RSFT  OSM(MOD_RSFT)
 #define TP_LALT  OSM(MOD_LALT)
@@ -34,16 +34,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_SPOT,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
-        ALT_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_MUTE,
-        TP_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_VOLU, KC_MPLY,
-        KC_LCTL, ALT_ESC, KC_LGUI,                            KC_SPC,                            TP_RALT, MO(1),   KC_MRWD, KC_VOLD, KC_MFFD
+        ALT_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
+        TP_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
+        KC_LCTL, ALT_ESC, KC_LGUI,                            KC_SPC,                             TP_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [1] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL,
-        _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, KC_PSCR, KC_SLCK, KC_PAUS, _______, KC_HOME,
-        _______, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, KC_MRWD, KC_MFFD, _______, _______, _______,          _______, KC_PGUP,
-        _______, RGB_TOG, _______, _______, _______, MD_BOOT, TG_NKRO, DBG_TOG, _______, _______, _______, _______,          KC_UP,   KC_PGDN,
-        _______, _______, _______,                            HK_COSL,                            _______, TG(1),   KC_LEFT, KC_DOWN, KC_RGHT
+        _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______,
+        _______, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, KC_MRWD, KC_MFFD, _______, _______, _______,          _______, KC_MUTE,
+        _______, RGB_TOG, _______, _______, _______, MD_BOOT, TG_NKRO, DBG_TOG, _______, _______, _______, _______,          KC_VOLU, KC_MPLY,
+        _______, _______, _______,                            HK_COSL,                            _______, _______, KC_MRWD, KC_VOLD, KC_MFFD
     ),
     /*
     [X] = LAYOUT(
